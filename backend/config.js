c5ff2ab57480e4e184c18e4fa262c5dfea637920
Zhbankova_Natalia <Natalek10.07@gmail.com>
2023-07-03 20:06:32 +0500

    ОтÐредактиреадактировала ÑконфигÐ

--- backend/config.js
+++ backend/config.js
@@ -1,13 +1,9 @@
-const {
-  NODE_ENV,
-  JWT_SECRET,
-  PORT,
-} = process.env;
+const { PORT = 3000, NODE_ENV, JWT_SECRET } = process.env;
 
 const config = {
   MONGODB_URI: 'mongodb://127.0.0.1:27017/mestodb',
-  PORT: NODE_ENV === 'production' ? PORT : 3000,
   JWT_SECRET: NODE_ENV === 'production' ? JWT_SECRET : 'dev-secret',
+  PORT,
 };
 
 module.exports = config;
